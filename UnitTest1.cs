27f61f704a1cbe88e2dfff76086a2b619c5debe7
TigDang <gizetdinov.m.f@gmail.com>
2022-04-16 15:48:06 +0300

    Add test1

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,6 +14,12 @@ namespace CountdownTest
         public void Test1()
         {
             var cntdwn = new CountDown();
+            var sbsc = new Subscriber(new CountDown[] { cntdwn });
+
+            cntdwn.Timer(100, "Some text");
+
+            cntdwn.RaiseCountDownEvent();
+
             Assert.Pass();
         }
     }
